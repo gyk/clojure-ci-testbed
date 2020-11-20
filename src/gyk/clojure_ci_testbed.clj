244ca31738c518a29f69bd052fe86ff9462010d5
Yukun Guo <gyk.net@gmail.com>
2020-11-20 17:28:14 +0800

    init

---
+++ src/gyk/clojure_ci_testbed.clj
@@ -0,0 +1,7 @@
+(ns gyk.clojure-ci-testbed
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/gyk/clojure_ci_testbed_test.clj
@@ -0,0 +1,7 @@
+(ns gyk.clojure-ci-testbed-test
+  (:require [clojure.test :refer :all]
+            [gyk.clojure-ci-testbed :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
