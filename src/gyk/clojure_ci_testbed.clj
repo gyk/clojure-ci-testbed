068585922eaa90c6c17047dd86f556e4963af25d
Yukun Guo <gyk.net@gmail.com>
2020-11-20 17:41:06 +0800

    implement add function

--- src/gyk/clojure_ci_testbed.clj
+++ src/gyk/clojure_ci_testbed.clj
@@ -1,7 +1,13 @@
 (ns gyk.clojure-ci-testbed
   (:gen-class))
 
+(defn add
+  "Adds two numbers."
+  [a b]
+  (+ a b))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (println (add 2 2)))
--- test/gyk/clojure_ci_testbed_test.clj
+++ test/gyk/clojure_ci_testbed_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [gyk.clojure-ci-testbed :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest two-plus-two
+  (testing "2 + 2"
+    (is (add 2 2) 4)))
